01315d10da1168c16f1a016be8dd68854a72918b
dash14 <dash14.ack@gmail.com>
2023-09-18 17:40:25 +0900

    Add notes

--- src/pages/Options.tsx
+++ src/pages/Options.tsx
@@ -5,11 +5,14 @@ import {
   Heading,
   HStack,
   Text,
+  UnorderedList,
+  ListItem,
 } from "@chakra-ui/react";
 import { css } from "@emotion/react";
 import { Brand } from "@/components/brand/Brand";
 import { BrandIcon } from "@/components/brand/BrandIcon";
 import { Copyright } from "@/components/brand/Copyright";
+import { ExternalLink } from "@/components/parts/ExternalLink";
 import { RuleSetsEdit } from "@/features/options/components/ruleset/RuleSetsEdit";
 import { useRequestBlockClient } from "@/hooks/useRequestBlockClient";
 import { useI18n } from "../hooks/useI18n";
@@ -57,6 +60,35 @@ function Options() {
 
           <RuleSetsEdit ruleSets={ruleSets} onChange={updateRuleSets} />
         </Box>
+      </Container>
+
+      <Container
+        maxW="930px"
+        marginY={4}
+        color="#444"
+        backgroundColor="#edf2f7"
+        fontSize={14}
+        paddingX={4}
+        paddingY={2}
+        borderRadius="0.5rem"
+      >
+        <Text as="div">Notes:</Text>
+        <UnorderedList>
+          <ListItem>
+            The network blocking and passing rules provided by this extension
+            utilize the chrome.declarativeNetRequest API. As such, the
+            limitations of this API still applied. See{" "}
+            <ExternalLink href="https://developer.chrome.com/docs/extensions/reference/declarativeNetRequest/#limits">
+              Rules limits
+            </ExternalLink>{" "}
+            in the API reference for more information about the limits.
+          </ListItem>
+          <ListItem>
+            Under no circumstances will the author of this extension,
+            dash14.ack, is not responsible for any loss, damage or inconvenience
+            caused by the use of this extension.
+          </ListItem>
+        </UnorderedList>
       </Container>
 
       <Container as="footer" textAlign="center" marginBottom={4}>
