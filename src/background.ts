5767fc94681eaf934931b1b32fc6aa1cf47c4663
dash14 <dash14.ack@gmail.com>
2023-09-23 23:58:41 +0900

    Fix action icon not updating after restarting Chrome

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,15 @@
 import { Main } from "./modules/worker/Main";
 
-const controller = new Main();
-controller.run();
+// When the extension is first installed,
+// when the extension is updated to a new version,
+// and when Chrome is updated to a new version
+chrome.runtime.onInstalled.addListener(function () {
+  const controller = new Main();
+  controller.run();
+});
+
+// When a user profile starts
+chrome.runtime.onStartup.addListener(function () {
+  const controller = new Main();
+  controller.run();
+});
