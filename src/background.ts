d40e139522e8300baaf9c4f216094e6f0c4ac866
dash14 <dash14.ack@gmail.com>
2023-09-25 20:39:48 +0900

    Change to open the options page on first startup

--- src/background.ts
+++ src/background.ts
@@ -3,13 +3,18 @@ import { Main } from "./modules/worker/Main";
 // When the extension is first installed,
 // when the extension is updated to a new version,
 // and when Chrome is updated to a new version
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener((details) => {
   const controller = new Main();
   controller.run();
+  if (details.reason === "install") {
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("options.html"),
+    });
+  }
 });
 
 // When a user profile starts
-chrome.runtime.onStartup.addListener(function () {
+chrome.runtime.onStartup.addListener(() => {
   const controller = new Main();
   controller.run();
 });
