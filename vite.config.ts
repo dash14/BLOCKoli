0407f1e515fa5a54145f720a41beecdea479838d
dash14 <dash14.ack@gmail.com>
2026-05-11 00:10:43 +0900

    chore: replace @vitejs/plugin-react-swc with @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { crx } from "@crxjs/vite-plugin";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import zipPack from "vite-plugin-zip-pack";
 import manifest from "./manifest.config";
